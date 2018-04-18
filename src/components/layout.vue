adb8587da591d4105505fa2e31ec610a42c4b059
zhenyun liang <493892240@qq.com>
2018-04-18 14:44:57 +0800

    第一次提交

---
+++ src/components/layout.vue
@@ -0,0 +1,23 @@
+<template>
+    <el-container>
+        <el-header class="layout_header">
+            <Nav />
+        </el-header>
+        <el-main>Main</el-main>
+    </el-container>
+</template>
+
+<script>
+import Nav from './nav'
+
+export default {
+    name: 'Layout',
+    components: {
+        Nav
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/nav.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="nav_box">
+        <router-link to="/" class="logo" tag="div">Liang</router-link>
+        <!--PC导航右边部分-->
+        <div class="nav_right">
+            <router-link to="/" tag="div">关于我</router-link>
+            <router-link to="/" tag="div">技术笔记</router-link>
+            <router-link to="/" tag="div">二次元</router-link>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Nav'
+}
+</script>
+
+<style>
+
+</style>
